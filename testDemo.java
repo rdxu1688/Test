dca5acd6c173975f9df87bc3d34ad9e194bad83f
rdxu1688 <ryx002@ucsd.edu>
2022-03-18 01:04:36 -0700

    file

---
+++ testDemo.java
@@ -0,0 +1,5 @@
+public class testDemo {
+    public static int add(int one, int two) {
+        return one+two;
+    }
+}
---
+++ testTest.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class testTest {
+    @Test
+    public void testAdd() {
+        assertEqual(3, testDemo.add(1,1));
+    }
+}
